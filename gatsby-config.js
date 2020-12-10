bfb3f968888a032a4243d2af88f53bb07289009b
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-12-10 19:55:33 +0900

    fix: gatsby-config.jsのpluginのカンマ抜け

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,7 +93,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    `gatsby-plugin-twitter`// this (optional) plugin enables Progressive Web App + Offline functionality
+    `gatsby-plugin-twitter`, // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-sitemap`,
